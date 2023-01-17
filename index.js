030f21c7c8d174f233db340f14546e583f45f80b
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-01-17 11:32:34 -0500

    commit tree 628c2254866a0e9c8c8abe6a4b38175c0013000f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.querySelector('main#main')
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML= "Yonina is the champion"
